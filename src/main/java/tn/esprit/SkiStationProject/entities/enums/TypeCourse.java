117412758b75a156615a32dc1ed426dc9478bc90
karimabbassi1902 <74004599+karimabbassi1902@users.noreply.github.com>
2023-10-15 18:52:07 +0100

    Delete src directory

--- src/main/java/tn/esprit/SkiStationProject/entities/enums/TypeCourse.java
+++
@@ -1,5 +0,0 @@
-package tn.esprit.SkiStationProject.entities.enums;
-
-public enum TypeCourse {
-    COLLECTIVE_CHILDREN, COLLECTIVE_ADULT, INDIVIDUAL
-}
--- src/main/java/tn/esprit/SkiStationProject/repositories/InstructorRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.SkiStationProject.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.esprit.SkiStationProject.entities.Instructor;
-
-public interface InstructorRepository extends JpaRepository<Instructor,Long> {
-
-
-}
